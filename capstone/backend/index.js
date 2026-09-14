9ca927887c7ef241396edd63ec858b563623df28
vikasthakurr <vikaskumar20012001@gmail.com>
2026-09-14 10:04:50 +0530

    backend code added

--- capstone/backend/index.js
+++ capstone/backend/index.js
@@ -1,5 +1,13 @@
+import dotenv from "dotenv";
+import connectDB from "./config/db.config.js";
 import app from "./src/app.js";
+dotenv.config();
 
-app.listen(3000, () => {
-  console.log("server is running on port 3000");
+const PORT = process.env.PORT || 5000;
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`API Docs: http://localhost:${PORT}/api-docs`);
+  });
 });
--- capstone/backend/src/app.js
+++ capstone/backend/src/app.js
@@ -1,11 +1,29 @@
 import express from "express";
 import connectDB from "../config/db.config.js";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 
 //redis connection
 
-const app = express();
+const app=express()
+
+app.use(helmet());
+
+// CORS
+app.use(cors({ origin: process.env.CLIENT_URL || "*", credentials: true }));
+
+// Logger
+app.use(morgan("dev"));
+
+// Body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.json({ message: "Cloud Kitchen API is running" });
+});
 
 //database connection...
 connectDB();
